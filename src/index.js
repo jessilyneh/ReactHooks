aa914d9c14c7c4fa7687495dab48973a6aa13449
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-27 23:08:32 -0300

    using component trees - stars rating part 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react"; //useState is a function, this is why i use { }
 import ReactDOM from "react-dom";
 import "./index.css";
+import {FaStar} from "react-icons/fa"
 
+const createArray = (length) => [
+  ...Array(length)]
+function Star({selected = false}) {
+  return <FaStar color={selected ? "yellow" : "gray"}/>
+}
+
+function StarRating(totalStars = 5) { //default value, in case totalStars hasnt a value defined in return
+  return createArray(totalStars).map((n,i) => (<Star key={i} />))
+}
 function App() {
-  const [check, setCheck] = useState(false);
-  return (
-    <div>
-      <input
-        type="checkbox"
-        value={check}
-        onChange={() => {
-          setCheck((check) => !check);
-        }}
-      />
-      <p>{check ? "checked" : "no checked"}</p>
-    </div>
-  );
+
+  return <StarRating totalStars={10}/>
 }
 
 ReactDOM.render(
