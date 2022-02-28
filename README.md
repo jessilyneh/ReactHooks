c1d9565a94557ac3e28a906ad84bdf37eac943b6
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-27 23:58:14 -0300

    send interactions up component trees

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ I left the steps commented out so the app doesn't break.<br/>
 With each new commit, I will test a different concept.<br/>
 Its simplier examples.<br/>
 
+#### run project
+npm start
+
+### content:
+
 - [array and object destructuring](https://github.com/Jeefelix/ReactHooks/commit/94e5c1bdaa197fd2092499f882052ed430691226)
 - [useState](https://github.com/Jeefelix/ReactHooks/commit/7e0a209625387a731d05a5f53b68abcb9cf24a73)
--- src/index.js
+++ src/index.js
@@ -6,28 +6,32 @@ import {FaStar} from "react-icons/fa"
 const createArray = (length) => [
   ...Array(length)]
 function Star({selected = false, onSelect}) {
-  return 
-  <FaStar 
-  color={selected ? "yellow" : "gray"}
-  onClick={onSelect}
-  />
+  return (
+    <FaStar 
+    color={selected ? "yellow" : "gray"}
+    onClick={onSelect}
+    />
+  )
 }
 
 function StarRating(totalStars = 5) { //default value, in case totalStars hasnt a value defined in return
   const [selectedStars, setSelectedStars] = useState(0)
-  return 
-  <>
-  {createArray(totalStars).map((n,i) => (
-  <Star 
-  key={i} 
-  selected={selectedStars > i} 
-  onSelect={()=> setSelectedStars(i + 1)}
-  />
+  return (
+    <>
+    {createArray(totalStars).map((_n,i) => (
+    <Star 
+    key={i} 
+    selected={selectedStars > i} 
+    onSelect={()=> setSelectedStars(i + 1)}
+    />
+      )
     )
+  }
+  <p>{selectedStars} of {totalStars}</p>
+    </>
   )
 }
-  <p>{selectedStars} of {totalStars}<p/>
-  </>
+
 function App() {
 
   return <StarRating totalStars={10}/>
@@ -38,4 +42,4 @@ ReactDOM.render(
     <App name="jess" />
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
