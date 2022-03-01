8071bf5c139774df2b0655b0820bd85b59c5186e
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-02-28 22:07:45 -0300

    Star Rating Fix - pass arguments by destructuring

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import React, { useState } from "react"; //useState is a function, this is why i use { }
+import React, {useState} from "react"; //useState is a function, this is why i use { }
 import ReactDOM from "react-dom";
 import "./index.css";
 import {FaStar} from "react-icons/fa"
 
 const createArray = (length) => [
-  ...Array(length)]
+  ...Array(length)];
+
 function Star({selected = false, onSelect}) {
   return (
-    <FaStar 
+    <FaStar
     color={selected ? "yellow" : "gray"}
     onClick={onSelect}
     />
   )
 }
 
-function StarRating(totalStars = 5) { //default value, in case totalStars hasnt a value defined in return
+function StarRating({totalStars = 5}) { //default value, in case totalStars hasnt a value defined in return
   const [selectedStars, setSelectedStars] = useState(0)
+
   return (
     <>
-    {createArray(totalStars).map((_n,i) => (
-    <Star 
-    key={i} 
-    selected={selectedStars > i} 
-    onSelect={()=> setSelectedStars(i + 1)}
-    />
+    {
+      createArray(totalStars).map((_n,i) => (
+        <Star 
+          key={i}
+          selected={selectedStars > i} 
+          onSelect={()=> setSelectedStars(i + 1)}
+        />
+        )
       )
-    )
-  }
-  <p>{selectedStars} of {totalStars}</p>
+    }
+    <p>{selectedStars} of {totalStars}</p>
     </>
   )
 }
@@ -39,7 +42,7 @@ function App() {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="jess" />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 )
