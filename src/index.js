f95cea33ada09ac0bc66baac6ff34a7bc6b233b9
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-01 14:48:24 -0300

    fetch data with useEffect

--- src/index.js
+++ src/index.js
@@ -2,32 +2,32 @@ import React, {useState, useEffect} from "react"; //useState is a function, this
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [name, setName] = useState("Jess")
-  const [role, setRole] = useState(false)
-
-  // runs after every update and render. Promote side effect
+export default function App() {
+  const [data, setData] = useState([]);
+  
+  // removing the dependency array, useEffect called
+  // the endpoint 732 times in a few seconds
   useEffect(() => {
-    document.title = `celebrate ${name}`
-    console.log(`celebrate ${name}`)
-  },[name]);//dependency array will change the behavior everytime "name" was change
+    fetch(`https://api.github.com/users`)
+      .then((response) => response.json())
+      .then(setData);
+    console.log("call API");
+  }, []);
 
-  useEffect(()=> {
-    console.log(`this person is:${role ? "javascript developer": "student"}`)
-  },[role]);//everytime the console.log changes is because role changes
-  return(
-    <section>
-      <p>Congratulations, {name}!</p>
-      <button
-       onClick={()=> setName("Rafael")}
-       >
-        Change winner
-      </button>
-      <p>{role ? "you are a developer" : "you are a student"}</p>
-      <button onClick={()=> setRole(true)}>become a <br/> professional developer <br/> in 18 weeks</button>
-    </section>
-  )
+  if (data) {
+    return (
+      <div>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+        <button onClick={() => setData([])}>Remove users</button>
+      </div>
+    );
+  }
 }
+
 
 ReactDOM.render(
   <React.StrictMode>
