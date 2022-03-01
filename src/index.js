88157e0e30b4e88ff2212c69c5a445ad13d59686
Jeefelix <37668064+Jeefelix@users.noreply.github.com>
2022-03-01 17:06:50 -0300

    useReducer - dispatch actions

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import React, {useReducer} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const initialState = {
+  message: "hello"
+};
+
+// send actions
+function reducer(state, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `HEY, my default message was ${state.message}`
+      };
+    case "yup":
+      return {
+        message: `YO, this is my default msg: ${state.message}`
+      };
+  }
+}
 export default function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked, false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div>
-    <input
-      type="checkbox"
-      value={checked}
-      onChange={toggle}
-    />
-    <p>{checked ? "checked" : "no checked"}</p>
-  </div>
-  )
+    <>
+      <p>Message:{state.message}</p>
+      <button onClick={() => dispatch({ type: "yell" })}>yell</button>
+      <button onClick={() => dispatch({ type: "yup" })}>yup</button>
+    </>
+  );
 }
 
 ReactDOM.render(
